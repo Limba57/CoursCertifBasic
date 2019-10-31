eeaa905b535c4eace66247bcbe8a242958cabc4b
Limba57 <limba@orange.fr>
2019-10-31 19:52:18 +0100

    Base et Main

---
+++ src/JavaClassStructure/Person.java
@@ -0,0 +1,18 @@
+package JavaClassStructure;
+
+public class Person {
+
+    private String firstname;
+    private String lastname;
+    private int age;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+
+}
---
+++ src/MainMethode/Main.java
@@ -0,0 +1,14 @@
+package MainMethode;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("arg-size :" + args.length);
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[ " + i + " ]= " + args[i]);
+        }
+
+    }
+}
