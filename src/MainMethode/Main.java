9a1c24de08a076200d3c1ef23a16e7c2e71686e8
Limba57 <limba@orange.fr>
2019-10-31 20:15:06 +0100

    comment

--- src/MainMethode/Main.java
+++ src/MainMethode/Main.java
@@ -1,14 +1,41 @@
 package MainMethode;
 
+/**
+ *
+ */
 public class Main {
 
+    /**
+     *this is the main method
+     *
+     * @param args comment line argument
+     */
     public static void main(String[] args) {
 
+        // printing size of argument
         System.out.println("arg-size :" + args.length);
 
+        /*
+        printing arguments
+         */
         for (int i = 0; i < args.length; i++) {
             System.out.println("args[ " + i + " ]= " + args[i]);
         }
 
+        System.out.println(sum(3, 4));
+
+
+    }
+
+    /**
+     *
+     * Calculate sum of two integer
+     *
+     * @param a first integer
+     * @param b second integer
+     * @return sum of the two number
+     */
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
